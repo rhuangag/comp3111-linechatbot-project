bb94f6d2e43f1ce30d003ad6f79a1e1563599371
Ivana MU <ymuaa1@gmail.com>
2017-11-02 16:00:11 +0800

    recommend_with_history

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Customer.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Customer.java
@@ -82,29 +82,91 @@ public class Customer{
 		
 		Vector<String> historyID = new Vector<String>();
 		Vector<String> recommendationID = new Vector<String>();//store all tourID first and remove historyID then
+		Vector<String> prefer_recommendationID = new Vector<String>();//according to history
 		String output;
 		
 		//Extract history-TourID and from the database
 		try {
 			Connection connection = KitchenSinkController.getConnection();
 			PreparedStatement stmt_history = connection.prepareStatement
-					("SELECT TourID from CustomerRecord where UserID "
-							+ "like concat('%', ?, '%')");
+					("SELECT TourID, DepartureDate, Duration, TourDescription from CustomerRecord where UserID like concat('%', ?, '%')");
 			stmt_history.setString(1, userID);
+			
+			// 0-->hot+spring 1-->mountain
+			boolean[] label = {false, false};
+			// 0--># of weekday, 1--># of weekend
+			int[] departureTime_number = {0, 0};
+			// 0--># of '2', 1--># of '3'
+			int[] duration_number = {0, 0};
+			
+			
 			ResultSet rs_history = stmt_history.executeQuery();
 			while(rs_history.next()) {
 				historyID.add(rs_history.getString("TourID"));
+				
+				if(calcDate(rs_history.getString("DepartureDate")) == "weekday")
+					departureTime_number[0]++;
+				else
+					departureTime_number[1]++;
+				
+				if(rs_history.getString("Duration") == "2")
+					duration_number[0]++;
+				else
+					duration_number[1]++;
+				if(label[0]==false)
+					label[0] = (rs_history.getString("TourDescription").toLowerCase().contains("hot") && rs_history.getString("TourDescription").toLowerCase().contains("spring"));
+				if(label[1]==false)
+					label[1] = rs_history.getString("TourDescription").toLowerCase().contains("mountain");
 			}
 			
-			//emmm...not sure
+			//criteria confirmation
+			String prefer_departure;
+			String prefer_duration;
+			if(departureTime_number[0]>=departureTime_number[1])
+				prefer_departure = "weekday";
+			else
+				prefer_departure = "weekend";
+			
+			if(duration_number[0]>=duration_number[1])
+				prefer_duration = "2";
+			else
+				prefer_duration = "3";
+			
 			
 			//Suppose the Tour List table in excel is named as TourList in db
 			PreparedStatement stmt = connection.prepareStatement
-					("SELECT TourID from TourList where UserID like concat('%',?,'%')");
+					("SELECT TourID, TourDescription, Duration, Date from TourList where UserID like concat('%',?,'%')");
 			stmt.setString(1, userID);
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()) {
 				recommendationID.add(rs.getString("TourID"));
+				if(rs.getString("Duration")==prefer_duration) {
+					prefer_recommendationID.add(rs.getString("TourID"));
+					continue;
+				}
+				if(prefer_departure=="weekend") {
+					if(rs.getString("Date").toLowerCase().contains("sat")||rs.getString("Date").toLowerCase().contains("sun")) {
+						prefer_recommendationID.add(rs.getString("TourID"));
+						continue;
+					}
+				}
+				if(label[0]==true) {
+					if(rs.getString("TourDescription").toLowerCase().contains("hot") && rs.getString("TourDescription").toLowerCase().contains("spring")) {
+						prefer_recommendationID.add(rs.getString("TourID"));
+						continue;
+					}
+					
+				}
+				if(label[1]==true) {
+					if(rs.getString("TourDescription").toLowerCase().contains("hot") && rs.getString("TourDescription").toLowerCase().contains("spring")) {
+						prefer_recommendationID.add(rs.getString("TourID"));	
+						continue;
+					}
+					
+				}
+				
+				
+				
 			}
 			
 			rs_history.close();
@@ -123,7 +185,14 @@ public class Customer{
 		}
 		
 		if(recommendationID.size() == 0) {
-			output = "Sorry, I have no more recommendation to you. Thanks for your support very much!";
+			output = "Sorry, I have no more recommendation to you. Thanks for your support very much.";
+		}
+		else if(prefer_recommendationID.size() != 0) {
+			Random rand = new Random(System.currentTimeMillis());
+			int position = rand.nextInt(prefer_recommendationID.size());
+			String outputID = prefer_recommendationID.get(position);
+			//select from db
+			output= Statement(outputID);			
 		}
 		else {
 			Random rand = new Random(System.currentTimeMillis());
@@ -145,7 +214,7 @@ public class Customer{
 		try {
 			Connection connection = KitchenSinkController.getConnection();
 			PreparedStatement stmt = connection.prepareStatement
-					("SELECT TourID, TourName, TourDescription, Days, Date, WeekendPrice, WeekdayPrice from TourList where UserID = "+ this.UserID +" AND TourID = "+ tourID +";");
+					("SELECT TourID, TourName, TourDescription, Days, Date, WeekendPrice, WeekdayPrice from TourList where UserID = "+ this.userID +" AND TourID = "+ tourID +";");
 			
 			ResultSet rs = stmt.executeQuery();
 			
@@ -164,6 +233,48 @@ public class Customer{
 			log.info("Exception while reading database: {}", e.toString());
 		}
 		return result;
+	}
+	
+	//helper function: convert the date to weekday/weekend
+	public String calcDate(String date) {
+		int index = 0;
+		int month = 0;
+		int day = 0;
+		int year = 0;
+		//day
+		for(; index < date.length(); index++) {
+			char temp = date.charAt(index);
+		
+			if(temp == '/') {
+				index++;
+				break;
+			}
+			else 
+				day = day*10 + Character.getNumericValue(temp);
+		}
+		//month
+		for(; index < date.length(); index++) {
+			char temp = date.charAt(index);
+		
+			if(temp == '/') {
+				index++;
+				break;
+			}
+			else 
+				month = month*10 + Character.getNumericValue(temp);
+		}
+		//year
+		for(; index < date.length(); index++) {
+			year = year*10 + Character.getNumericValue(date.charAt(index));
+		}
+		
+		Calendar c = Calendar.getInstance();
+		c.set(year, month-1, day);
+		
+		if((c.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)||(c.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY))
+			return "weekend";
+		else
+			return "weekday";
 	}
 	//TODO
 	//Cancel the booking in the database, mark the cancellation in the customer record
