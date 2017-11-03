87c5aab85c691f47e377fb9f2bda941961f81bac
walterwu <walterwu@walterwudeMacBook-Air.local>
2017-11-03 21:44:58 +0800

    finalize

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Filter.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Filter.java
@@ -258,7 +258,7 @@ public class Filter {
 		}
 		
 		PreparedStatement findConfirmedButAcceptBooking = connection.prepareStatement
-				("SELECT  departuredate from bookingtable where status='availiable' and tourID =?");
+				("SELECT  departuredate from bookingtable where status='confrimed1' and tourID =?");
 		findConfirmedButAcceptBooking.setString(1, TourID);
 		ResultSet rsFindConfirmedButAcceptBooking=findConfirmedButAcceptBooking.executeQuery();
 		String confirmedButAcceptBook="";
