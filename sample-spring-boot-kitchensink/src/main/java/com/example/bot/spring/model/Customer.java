68c299cfe6eae7a893b84ed0f0d428dd0013186e
Ivana MU <ymuaa1@gmail.com>
2017-11-03 19:44:04 +0800

    a

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Customer.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Customer.java
@@ -134,9 +134,9 @@ public class Customer{
 			else
 				prefer_departure = "weekday";
 			
-			if(((duration_number[0]==0) && (duration_number[1]==0)) || (duration_number[0] == duration_number[1]))
-				prefer_duration = "0";
-			else if(duration_number[0]>duration_number[1])
+			//if(((duration_number[0]==0) && (duration_number[1]==0)) || (duration_number[0] == duration_number[1]))
+			//	prefer_duration = "0";
+			if(duration_number[0]>duration_number[1])
 				prefer_duration = "2";
 			else
 				prefer_duration = "3";
