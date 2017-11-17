61f6aee90dc051e21df9d3db7e99b431f0fc96a3
Ivana MU <ymuaa1@gmail.com>
2017-11-17 18:04:53 +0800

    lalala

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Notification.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Notification.java
@@ -37,7 +37,8 @@ public class Notification implements Observer{
 		if(time[3]=="10") {
 			currentDate = time[2]+"/"+time[1]+"/"+time[0];
 			NotifyStatus();
-			promotionStatus(time[0],time[1],time[2]);
+			//promotionStatus(time[0],time[1],time[2]);
+			pushPromotion();
 
 		}
 
