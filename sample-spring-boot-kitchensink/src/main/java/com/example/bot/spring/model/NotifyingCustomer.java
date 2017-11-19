faf3fca3151310a9a302f9f32939cec6de4f3b56
Ivana MU <ymuaa1@gmail.com>
2017-11-19 22:38:14 +0800

    aaa

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/NotifyingCustomer.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/NotifyingCustomer.java
@@ -57,7 +57,7 @@ public class NotifyingCustomer implements Observer{
 
 		}
 		
-		//pushPromotion();
+		pushPromotion();
 
 	}
 
@@ -69,8 +69,18 @@ public class NotifyingCustomer implements Observer{
 		String imageUrl = KitchenSinkController.createUri("/static/promotion/join-now.jpg");
         CarouselTemplate carouselTemplate = new CarouselTemplate(
                 Arrays.asList(
-                        new CarouselColumn(imageUrl, "promotion", "come and join us", null),
-                        new CarouselColumn(imageUrl, "promotion", "emmmm", null)
+                		new CarouselColumn(imageUrl, "promotion", "come and join us", Arrays.asList(
+                                new URIAction("Go to line.me",
+                                              "https://line.me"),
+                                new PostbackAction("Say hello1",
+                                                   "promotion")
+                                )),
+                        new CarouselColumn(imageUrl, "promotion", "emmmm", Arrays.asList(
+                                new URIAction("Go to line.me",
+                                        "https://line.me"),
+                          new PostbackAction("Say hello1",
+                                             "promotion")
+                          ))
                 ));
         TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
 		
