7f23d2bcb78a63479cc5f1cb1b8391bd841dc4ca
yezy <yezy@yezytekiMacBook-Air.local>
2017-11-19 14:26:15 +0800

    add test case for discount

---
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/DiscountTester.java
@@ -0,0 +1,105 @@
+package com.example.bot.spring;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.google.common.io.ByteStreams;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.FollowEvent;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.MessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.time.*;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.time.format.DateTimeFormatter;
+
+
+import com.example.bot.spring.Discount;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { CustomerTester.class})
+public class DiscountTester {
+    @Test
+    public void testUpdate1() {
+    	boolean thrown = false;
+		Discount tester = new Discount();
+		TimeManager tm = TimeManager.getTimer();
+		
+		ZoneId currentZone = ZoneId.of("Asia/Shanghai");
+        ZonedDateTime zonedNow = ZonedDateTime.now(currentZone);
+        ZonedDateTime target = zonedNow.withYear(2017).withMonth(11).withDayOfMonth(19).withHour(14).withMinute(0);
+        
+		tm.addObserver(tester);
+		
+        int result = 0;
+		try {
+			tm.setZonedDateTime(target);
+			tm.testNotify();
+			result = tester.inupdate1;
+    	 	}catch(Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown).isEqualTo(true);
+		assertThat(result).isEqualTo(1);
+    }
+    
+    @Test
+    public void testUpdate2() {
+    	boolean thrown = false;
+		Discount tester = new Discount();
+		TimeManager tm = TimeManager.getTimer();
+		
+		ZoneId currentZone = ZoneId.of("Asia/Shanghai");
+        ZonedDateTime zonedNow = ZonedDateTime.now(currentZone);
+        ZonedDateTime target = zonedNow.withYear(2002).withMonth(1).withDayOfMonth(1).withHour(1).withMinute(0);
+        
+		tm.addObserver(tester);
+		
+        int result = 0;
+		try {
+			tm.setZonedDateTime(target);
+			tm.testNotify();
+			result = tester.inupdate2;
+    	 	}catch(Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown).isEqualTo(true);
+		assertThat(result).isEqualTo(1);
+    }
+}
