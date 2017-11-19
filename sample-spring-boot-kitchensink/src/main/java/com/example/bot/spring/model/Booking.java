e9018efe84ba1a100e5a136df2ab6755873e6b67
Gao Xiaoyi <31607936+xgaoak@users.noreply.github.com>
2017-11-19 21:15:09 +0800

    fdaf

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Booking.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/model/Booking.java
@@ -263,8 +263,8 @@ public class Booking {
 			ResultSet datas = Querydata.executeQuery();
 			datas.next();
 			PreparedStatement QueryMax = connection.prepareStatement("Select tourcapacity, currentcustomer"
-					+ " from bookingtable where tourid like " + datas.getString(2) + " and departuredate like "
-					+ datas.getString(3));
+					+ " from bookingtable where tourid like '" + datas.getString(2) + "' and departuredate like '"
+					+ datas.getString(3) +"'");
 			ResultSet moredata = QueryMax.executeQuery();
 			moredata.next();
 			if ((moredata.getInt(2)+datas.getInt(7)+datas.getInt(8)+ Integer.parseInt(numberOfToodlers)) > moredata.getInt(1))
